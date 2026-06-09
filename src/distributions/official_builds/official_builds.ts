11be39b584018f60ca60b1a93660e60007fed9fa
priya-kinthali <priya-kinthali@github.com>
2026-06-09 15:14:28 +0530

    with else log

--- src/distributions/official_builds/official_builds.ts
+++ src/distributions/official_builds/official_builds.ts
@@ -111,14 +111,15 @@ export default class OfficialBuilds extends BaseDistribution {
         core.info(
           `Received HTTP status code ${err.httpStatusCode}. This usually indicates the rate limit has been exceeded`
         );
-      } else if (
-        err instanceof hc.HttpClientError &&
-        (err.statusCode === 403 || err.statusCode === 429)
-      ) {
-        core.info(
-          `Received HTTP status code ${err.statusCode}. This usually indicates the rate limit has been exceeded`
-        );
+        // } else if (
+        //   err instanceof hc.HttpClientError &&
+        //   (err.statusCode === 403 || err.statusCode === 429)
+        // ) {
+        //   core.info(
+        //     `Received HTTP status code ${err.statusCode}. This usually indicates the rate limit has been exceeded`
+        //   );
       } else {
+        core.info('From else block...');
         core.info((err as Error).message);
       }
       core.debug((err as Error).stack ?? 'empty stack');
